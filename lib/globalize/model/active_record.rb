e540d8eaf085de7edff0a6dbda7544250b974a51
Franck <franck@imac-de-franck.(none)>
2009-05-14 12:21:55 +0200

    adding method #{attr_name}_locale(locale) to be able to force locale for an attribute

--- lib/globalize/model/active_record.rb
+++ lib/globalize/model/active_record.rb
@@ -26,9 +26,15 @@ module Globalize
             klass.send :define_method, attr_name, lambda {
               globalize.fetch self.class.locale, attr_name
             }
+
+            klass.send :define_method, "#{attr_name}_locale", lambda {|locale|
+              globalize.fetch locale, attr_name
+            }
+            
             klass.send :define_method, "#{attr_name}_before_type_cast", lambda {
               globalize.fetch self.class.locale, attr_name
-            }
+            }        
+            
             klass.send :define_method, "#{attr_name}=", lambda {|val|
               globalize.stash self.class.locale, attr_name, val
               self[attr_name] = val
